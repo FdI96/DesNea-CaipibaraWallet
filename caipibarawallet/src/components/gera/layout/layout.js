cb19ebb707ef24ea0e11581e542dae0f15a10c6b
Gerardo <gerardot2@gmail.com>
2022-02-24 18:02:22 -0300

    Primer commit gera

---
+++ caipibarawallet/src/components/gera/layout/layout.js
@@ -0,0 +1,14 @@
+import Footer from '../footer/footer'
+import Navbar from '../navbar/navbar'
+
+function layout()
+{
+    return (
+        <div id="Layout">
+        <Navbar/>
+        <Footer/>
+    </div>
+    )
+}
+
+export default layout
