0e3760d77b5c47b7ab6ac2bacbe065829b638e94
Federico Ignacio López Cechini <federico.lopezcechini@desarrollosnea.com>
2022-02-24 17:50:42 -0300

    Showing off our skills in react by using composition and inheritance concepts

---
+++ caipibarawallet/src/components/layout/layout.js
@@ -0,0 +1,15 @@
+import NavBar from "../navbar/navbar";
+import Footer from "../footer/footer";
+
+function Layout(props) {
+    return (
+      <div id='Layout'>
+          <NavBar/>
+            {props.children}
+          <Footer/>
+      </div>
+    );
+  }
+  
+  export default Layout;
+  
