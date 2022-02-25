a6f14cea9f5385b2003b5e58feab6121df46029a
Marcos <marcos_fab182@hotmail.com>
2022-02-25 16:36:04 -0300

    remove package-lock

--- caipibarawallet/src/App.js
+++ caipibarawallet/src/App.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import logo from './assets/logo.svg';
-import './App.css';
-import Layout from './components/dario/layout/layout'
-import Body from './components/dario/layout/body/body'
-=======
 import "./App.css";
 import Layout from "./components/layout/layout";
 import Body from "./components/body/body";
->>>>>>> 0fb9bb7ecbe911910504f7450bd1eac6d26392a0
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-       <Layout>
-          <Body />
-       </Layout>
-=======
       <Layout>
         <Body />
       </Layout>
->>>>>>> 0fb9bb7ecbe911910504f7450bd1eac6d26392a0
     </div>
   );
 }
