bf3dbae0b10012ceb67b0d67db569ce06e2f4654
Dario Parra <dario.parra@desarrollosnea.com>
2022-03-02 16:18:22 -0300

    changes dario

--- caipibarawallet/src/App.js
+++ caipibarawallet/src/App.js
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
+import * as React from 'react'
 import logo from './assets/logo.svg';
-import './App.css';
-import Layout from './components/dario/layout/layout'
-import Body from './components/dario/layout/body/body'
-=======
 import "./App.css";
-import Layout from "./components/layout/layout";
-import Body from "./components/body/body";
->>>>>>> 0fb9bb7ecbe911910504f7450bd1eac6d26392a0
+import Layout from "./components/dario/layout/layout";
+import Body from "./components/dario/body/body";
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-       <Layout>
-          <Body />
-       </Layout>
-=======
-      <Layout>
-        <Body />
-      </Layout>
->>>>>>> 0fb9bb7ecbe911910504f7450bd1eac6d26392a0
+        <Layout>
+            <Body />
+        </Layout>
     </div>
   );
 }
---
+++ caipibarawallet/src/components/dario/body/body.js
@@ -0,0 +1,85 @@
+import { tab } from "@testing-library/user-event/dist/tab";
+import { useEffect, useState } from "react"
+import "./body.css"
+
+function Body(props) {
+
+    const [operations, setOperation] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3004/operations', {
+            method: 'GET'
+        }).then((response) => {
+            return response.json();
+        }).then((result) => {
+            setOperation(result);
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, []);
+
+    const addOperation = () => {
+        var data = {
+            id: 7,
+            concept: "algo",
+            amount: "algo",
+            date: "20200225",
+            type: "income"
+        }
+        fetch('http://localhost:3004/operations/', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+            
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const editOperation = () => {
+        console.log('edit operation');
+    }
+
+    const deleteOperation = () => {
+        console.log('delete operation');
+    }
+
+
+    return (
+        <div id="body">
+            <h4>List of Operations</h4>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Concept</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                        <th>Type</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {operations.map((card, index) => {
+                        return (
+                            <tr>
+                            <td>{card.concept}</td>
+                            <td>{card.amount}</td>
+                            <td>{card.date}</td>
+                            <td>{card.type}</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        )
+                        
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Body
