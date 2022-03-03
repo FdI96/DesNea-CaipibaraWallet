b900d391f2658db72ac6eb782b545a57ed515956
Federico Ignacio López Cechini <federico.lopezcechini@desarrollosnea.com>
2022-03-03 16:58:29 -0300

    Add styles to my home page and build the keypad

--- caipibarawallet/src/components/fede/body/body.js
+++ caipibarawallet/src/components/fede/body/body.js
@@ -1,38 +1,27 @@
 import "./body.css"
+import ShowRecords from "../showRecords/showRecords";
 
 function Body() {
-    return (
-      <div id='body-fede'>
-        <div className='operation-box'>
-          <h2 className='register-operation-title'>Register a new operation here:</h2>
-          <div className='register-operation-content'>
-            <form>
-              <label>
-                Concept:
-                <input type="text" name="concept" />
-              </label>
-              <label>
-                Amount:
-                <input type="text" name="amount" />
-              </label>
-              <label>
-                Date:
-                <input type="text" name="date" />
-              </label>
-              <label>
-                Type:
-                <input type="text" name="type" />
-              </label>
-              <input type="submit" value="Submit" />
-            </form>
+  
+
+  return (
+    <div id='body-fede'>
+      <div className='operation-box'>
+        <div className="operations-and-records">
+          <div className='select-operations'>
+            <h2 className='register-operation-title'>Select an operation:</h2>
+            <button className="operation-btn">Register a new operation.</button>
+            <button className="operation-btn">Edit an operation.</button>
+            <button className="operation-btn">Delete an operation.</button>
+            <button className="operation-btn"> Display all the records.</button>
           </div>
-        </div>
-        <div>
-          
+          <ShowRecords/>
         </div>
       </div>
+        
+    </div>
     );
-  }
+}
   
   export default Body;
   
---
+++ caipibarawallet/src/components/fede/showRecords/showRecords.js
@@ -0,0 +1,9 @@
+import './showRecords.css'
+import axiosRequest from '../../../request'
+export default function ShowRecords() {
+    return (
+        <div id='show-records-element'>
+            <h1>I'm listing the last 10 records</h1>
+        </div>
+    )
+}
