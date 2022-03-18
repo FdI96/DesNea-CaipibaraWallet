c5d1c0e020cd18db2e6c92f52ab144941c7e759d
Federico Ignacio López Cechini <federico.lopezcechini@desarrollosnea.com>
2022-03-18 16:50:50 -0300

    Use the store and the dispatch

--- caipibarawallet/src/components/fede/body/body.js
+++ caipibarawallet/src/components/fede/body/body.js
@@ -1,14 +1,18 @@
 import "./body.css"
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import ShowRecords from "../Records/showRecords/showRecords";
 import axiosRequest from '../../../request'
 import RegisterRecords from "../Records/registerRecords/registerRecords";
 import DeleteRecords from "../Records/deleteRecords/deleteRecords";
 import EditRecords from "../Records/editRecords/editRecords";
+import { useDispatch, useSelector } from "react-redux";
+import { increment } from "../Records/reducer/fedeReducer";
 // add this in a dinamyc way
 const urlServer = 'http://localhost:3030/'
 
 function Body() {
+
+  const dispatch = useDispatch()
   const [component, setComponent] = useState('records')
 
   const [records, setRecords] = useState([])
@@ -47,16 +51,30 @@ function Body() {
     }
   }
 
+  const WinOneDollar = () => {
+    dispatch(increment())
+  }
+
+  var balance = useSelector((state) => state.fede.myMoney)
+
   return (
     <div id='body-fede'>
       <div className='operation-box'>
         <div className="operations-and-records">
-          <div className='select-operations'>
+          <div className='left-panel'>
+            <div className="random-test" onClick={() => WinOneDollar()}>Win one 1$ just clicking</div>
+            <div className="balance-box">
+              <div id="balance">Balance:</div>
+              <div id="balance-amount">{balance}</div>     
+            </div>
             <h2 className='register-operation-title'>Select an operation:</h2>
-            <button className="operation-btn" onClick={() => setComponent('register')}>Register a new operation.</button>
-            <button className="operation-btn" onClick={() => setComponent('edit')}>Edit an operation.</button>
-            <button className="operation-btn" onClick={() => setComponent('delete')}>Delete an operation.</button>
-            <button className="operation-btn" onClick={() => setComponent('records')}> Display the latest records.</button>
+            <div className="select-operations">
+              <button className="operation-btn" onClick={() => setComponent('register')}>Register a new operation.</button>
+              <button className="operation-btn" onClick={() => setComponent('edit')}>Edit an operation.</button>
+              <button className="operation-btn" onClick={() => setComponent('delete')}>Delete an operation.</button>
+              <button className="operation-btn" onClick={() => setComponent('records')}> Display the latest records.</button>
+            </div>
+            
           </div>
           <div id='display-box'>
             {switchRenders()}
