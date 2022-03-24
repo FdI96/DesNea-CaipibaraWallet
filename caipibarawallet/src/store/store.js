56d53eac0550f99987a7fd975735f02f0238a5a6
Victor Sanchez <victor_sanchez@outlook.com>
2022-03-24 18:30:26 -0300

    reducers

--- caipibarawallet/src/store/store.js
+++ caipibarawallet/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import fedeReducer from '../components/fede/Records/reducer/fedeReducer'
+import victorReducer from '../components/victor/reducer/victorReducer'
 
 export default configureStore({
   reducer: {
-    fede: fedeReducer
+    fede: fedeReducer,
+    victor: victorReducer
   }
 })
