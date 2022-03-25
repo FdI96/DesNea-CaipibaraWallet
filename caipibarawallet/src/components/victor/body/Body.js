9dafddaf5c7bb4c0262ecaf15e4661bfd96b0251
Victor Sanchez <victor_sanchez@outlook.com>
2022-03-25 20:09:41 -0300

    redux stuff

--- caipibarawallet/src/components/victor/body/Body.js
+++ caipibarawallet/src/components/victor/body/Body.js
@@ -2,11 +2,34 @@ import React from 'react'
 import './body.css'
 
 import { Link } from 'react-router-dom'
-
+import { useDispatch, useSelector } from "react-redux";
+import { increment } from "../reducer/victorReducer";
 import CapiLogo from '../capiLogo/CapiLogo'
 import Card from '../card/Card'
 
 export default function Body() {
+  
+  // const switchRenders = () => {
+  //   switch (component) {
+  //     case 'myHome':
+  //       return <ShowRecords records={records}/>
+  //     case 'crypto':
+  //       return <RegisterRecords />
+  //     case 'delete':
+  //       return <DeleteRecords />
+  //     case 'edit':
+  //       return <EditRecords />
+  //     default:
+  //       return <ShowRecords records={records}/>
+  //   }
+  // }
+
+  const counterOnClick = () => {
+    dispatch(increment())
+  }
+
+  var balance = useSelector((state) => state.victor.myMoney)
+
   return (
     <div id={'body-victor'}>
       <Card>
--- caipibarawallet/src/components/victor/card/Card.js
+++ caipibarawallet/src/components/victor/card/Card.js
@@ -3,7 +3,7 @@ import './card.css'
 
 export default function Card(props) {
   return (
-    <div className={'card center'}>
+    <div className={'card bg-red-500'}>
       {props.children}
     </div>
   )
