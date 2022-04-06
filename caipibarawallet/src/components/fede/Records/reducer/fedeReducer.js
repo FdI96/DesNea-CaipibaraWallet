87180431bc9b263fef78500f0455ae4ecf127807
Federico Ignacio López Cechini <federico.lopezcechini@desarrollosnea.com>
2022-04-06 16:22:18 -0300

    Set up Slice Reducer

--- caipibarawallet/src/components/fede/Records/reducer/fedeReducer.js
+++ caipibarawallet/src/components/fede/Records/reducer/fedeReducer.js
@@ -1,10 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-export const fedeReducer = createSlice({
+const server = 'http://localhost:3030/'
+
+const initialState = {
+  myMoney: 999,
+  records:[],
+  status: 'idle',
+  error: null
+}
+
+export const fedeSlice = createSlice({
   name: 'fede',
-  initialState: {
-    myMoney: 999,
-  },
+  initialState,
   reducers: {
     increment: (state) => {
       state.myMoney = state.myMoney + 1
@@ -15,10 +22,44 @@ export const fedeReducer = createSlice({
     incrementAmount: (state, amount) => {
       state.myMoney = state + amount
     },
+    storeRecords: (state, records) => {
+      state.records = records
+    }
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRecords.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchRecords.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.records = state.records.concat(action.payload)
+      })
+      .addCase(fetchRecords.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  }
+
+
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementAmount } = fedeReducer.actions
+export const { increment, decrement, incrementAmount } = fedeSlice.actions
 
-export default fedeReducer.reducer
+export default fedeSlice.reducer
+
+export const selectStatus = state => state.fede.status
+
+export const selectAllRecords = state => state.fede.records
+
+export const selectRecordById = (state, recordId) => state.fede.records.find(record => record.id === recordId)
+
+const getRecords = async () => {
+  
+  const response = await server.get('operations')
+  return response.data
+}
+
+export const fetchRecords = createAsyncThunk('records/fetchRecords', getRecords())
--- caipibarawallet/src/store/store.js
+++ caipibarawallet/src/store/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import fedeReducer from '../components/fede/Records/reducer/fedeReducer'
+import fedeSlice from '../components/fede/Records/reducer/fedeReducer'
 import darioReducer from '../components/dario/reducer/darioReducer'
 
 export default configureStore({
   reducer: {
-    fede: fedeReducer,
+    fede: fedeSlice,
     dario: darioReducer
   }
 })
